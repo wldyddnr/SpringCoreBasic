0d67962790885b0604fb8b49f9545c28bd013a68
yonguk <wldyddnr@gmail.com>
2023-03-16 02:26:55 +0900

    싱글톤예제

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -9,9 +9,9 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class BeanDefinitionTest {
 
-//    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
-    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.Xml");
+//    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.Xml");
 
     @Test
     @DisplayName("빈 설정 메타정보 확인")
