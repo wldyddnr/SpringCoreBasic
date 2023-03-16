d0d652592e6fe4e531b9ade9c8a84c244db8ef43
yonguk <wldyddnr@gmail.com>
2023-03-17 01:24:36 +0900

    컴포넌트스캔 예제

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -6,7 +6,7 @@ public class StatefulService {
 
     public void order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price; //여기가 문제!
+        this.price = price; //여기가 문제
     }
 
     public int getPrice() {
